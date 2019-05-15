35869ee63d0a4e2a303c8315ce7558d0e33fd577
Javier Erazo <alexerazoajebsk8@outlook.com>
2019-05-15 10:50:14 -0500

    CambioconSWITCHCase

--- SolCalculadoraTutorial/CalculadoraTutorial/CalculadoraTutorial.cpp
+++ SolCalculadoraTutorial/CalculadoraTutorial/CalculadoraTutorial.cpp
@@ -1,12 +1,26 @@
 #include"pch.h"
 #include <iostream>
+#include"Calculator.h"
+#include<conio.h>
 
 using namespace std;
 
 int main()
 {
+	double x = 0, y = 0 , resultado = 0;
+	
+	char oper = '\0';
+
 	cout<< "Calculator Console Application" << endl << endl;
-	cout<< "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
-		<< endl;
+	cout<< "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
+	while(true)	
+	{
+	cin >> x >> oper >> y;
+
+	Calculator  Calculate;
+	resultado = Calculate.Calculate(x, oper, y);
+	cout << resultado;
+	
+	}
 	return 0;
 }
--- SolCalculadoraTutorial/CalculadoraTutorial/Calculator.cpp
+++ SolCalculadoraTutorial/CalculadoraTutorial/Calculator.cpp
@@ -24,7 +24,9 @@ double Calculator::Calculate(double x, char oper, double y)
 		return x * y;
 	case '/':
 		return x / y;
+	
 	default:
 		return 0.0;
+
 	}
 }
